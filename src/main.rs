f61db5c0efc0e444aa1fe3922270a02e939c3fb4
multinerd <admin@multinerd.io>
2025-04-11 06:30:11 -0400

    reverted to original code to fix errors

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,28 @@ use std::time::Duration;
 
 fn main() -> Result<()> {
     let gateway = std::env::var("NATPMP_GATEWAY_IP").unwrap_or("10.2.0.1".to_owned());
-    let mut n = Natpmp::new_with(gateway.parse().unwrap()).expect("Parsing gateway address failed!");
+    let mut n =
+    Natpmp::new_with((&gateway).parse().unwrap()).expect("Parsing gateway address failed!");
     let mut client = Client::default();
 
     let _ = query_gateway(&mut n).expect("Quering Public IP failed!");
 
     let mut mr = query_available_port(&mut n).expect("Quering a Port Mapping failed!");
-    update_qbittorrent(&mut client, mr.public_port()).expect("Failed to update qBittorrent.");
+    update_qbittorrent(&mut client, mr.public_port()).expect("Failed to update QBittorrent.");
 
     loop {
-        thread::sleep(*mr.lifetime() / 2); //Renew at half lifetime
+        thread::sleep(mr.lifetime().clone() / 2); //Renew at half lifetime
         let mr_ = query_port(&mut n, mr.private_port(), mr.public_port(), true)
-            .or(query_available_port(&mut n))
-            .expect("Every renewal method failed!");
+        .or(query_available_port(&mut n))
+        .expect("Every renewal method failed!");
         if mr.public_port() != mr_.public_port() {
             println!("Port has changed, setting incoming port on QBittorrent...");
-            update_qbittorrent(&mut client, mr.public_port()).expect("Failed to update QBittorrent.");
+            update_qbittorrent(&mut client, mr.public_port())
+            .expect("Failed to update QBittorrent.");
         }
         mr = mr_;
     }
+
     Ok(())
 }
 
@@ -35,16 +38,17 @@ fn update_qbittorrent(client: &mut Client, port: u16) -> Result<()> {
     .form(&[("json", &format!(r#"{{"listen_port":{}}}"#, port))])
     .send()?
     .error_for_status()?;
+    Ok(())
 }
 
 fn query_gateway(n: &mut Natpmp) -> Result<GatewayResponse> {
     let mut timeout = 250;
     while timeout <= 64000 {
         n.send_public_address_request()
-            .map_err(|err| anyhow!("Fail with {:?}", err))?;
+        .map_err(|err| anyhow!("Fail with {:?}", err))?;
         println!(
             "Public address request sent! (will timeout in {}ms)",
-            timeout
+                 timeout
         );
         thread::sleep(Duration::from_millis(timeout));
         match n.read_response_or_retry() {
@@ -56,7 +60,7 @@ fn query_gateway(n: &mut Natpmp) -> Result<GatewayResponse> {
                 println!(
                     "Got response: IP: {}, Epoch: {}",
                     gr.public_address(),
-                    gr.epoch()
+                         gr.epoch()
                 );
                 return Ok(gr);
             }
@@ -70,7 +74,7 @@ fn query_gateway(n: &mut Natpmp) -> Result<GatewayResponse> {
 }
 
 fn query_available_port(n: &mut Natpmp) -> Result<MappingResponse> {
-    query_port(n, 0, 0, false)
+    return query_port(n, 0, 0, false);
 }
 
 fn query_port(
@@ -81,9 +85,8 @@ fn query_port(
 ) -> Result<MappingResponse> {
     let mut timeout = 250;
     while timeout <= 64000 {
-        let _ = n
-            .send_port_mapping_request(Protocol::TCP, 0, 0, 360)
-            .map_err(|err| anyhow!("Fail with {:?}", err));
+        let _ = n.send_port_mapping_request(Protocol::TCP, 0, 0, 360)
+        .map_err(|err| anyhow!("Fail with {:?}", err));
         println!("Port mapping request sent! (will timeout in {}ms)", timeout);
 
         // sleep for a while
@@ -97,18 +100,18 @@ fn query_port(
                 println!(
                     "Got response: Internal: {}, External: {}, Lifetime: {}s",
                     tr.private_port(),
-                    tr.public_port(),
-                    tr.lifetime().as_secs()
+                         tr.public_port(),
+                         tr.lifetime().as_secs()
                 );
                 if (!check)
                     || (tr.private_port() == internal
-                        && tr.public_port() == external
-                        && tr.lifetime().as_secs() > 0)
-                {
-                    return Ok(tr);
-                } else {
-                    println!("Retring, port is not the one wanted!");
-                }
+                    && tr.public_port() == external
+                    && tr.lifetime().as_secs() > 0)
+                    {
+                        return Ok(tr);
+                    } else {
+                        println!("Retring, port is not the one wanted!");
+                    }
             }
             _ => {
                 bail!("Expecting a tcp response");
